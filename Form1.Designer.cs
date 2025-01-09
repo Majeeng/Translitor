9b22a8a536f9ed7ea3db832c67c2701b52ea4f9e
Alexandr <eph.sasha@gmail.com>
2025-01-09 18:17:11 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace translitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbConclusion = new System.Windows.Forms.RichTextBox();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(135, 48);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выйтиToolStripMenuItem.Text = "Закрыть";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // tbConclusion
+            // 
+            this.tbConclusion.Font = new System.Drawing.Font("Calibri", 14F);
+            this.tbConclusion.Location = new System.Drawing.Point(10, 28);
+            this.tbConclusion.Margin = new System.Windows.Forms.Padding(4);
+            this.tbConclusion.Name = "tbConclusion";
+            this.tbConclusion.Size = new System.Drawing.Size(301, 149);
+            this.tbConclusion.TabIndex = 3;
+            this.tbConclusion.Text = "";
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnMinimize.BackgroundImage = global::translitor.Properties.Resources.tire;
+            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMinimize.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMinimize.Location = new System.Drawing.Point(284, 2);
+            this.btnMinimize.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(32, 19);
+            this.btnMinimize.TabIndex = 5;
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 14F);
+            this.label1.Location = new System.Drawing.Point(10, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 64);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1. Выделите нужный текст 2. Нажмите: Shift+Alt+P";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 254);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.tbConclusion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Deactivate += new System.EventHandler(this.MainForm_Deactivate);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox tbConclusion;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace translitor
+{
+    public partial class MainForm : System.Windows.Forms.Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            notifyIcon.Visible = true;
+
+            // регистрация глобальной комбинации клавиш
+            FormClosing += (s, e) => { UnregisterHotKey(Handle, HOTKEY_ID); };
+            RegisterHotKey(Handle, HOTKEY_ID, MOD_ALT | MOD_SHIFT, (int)Keys.P);
+        }
+
+        #region комбинации клавиш
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int MOD_ALT = 0x0001;
+        private const int MOD_SHIFT = 0x0004;
+
+        private const int WM_HOTKEY = 0x0312;
+
+        private const int HOTKEY_ID = 1;
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY)
+            {
+                int id = m.WParam.ToInt32();
+
+                if (id == HOTKEY_ID)
+                {
+                    //perform hotkey action
+                    Thread.Sleep(300);
+                    translite();
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+        #endregion
+
+        SoundPlayer sp = new SoundPlayer(Properties.Resources.empty);
+
+        #region перевод
+        #region получение выделенного текста
+        #region импорт функций
+        [DllImport("user32.dll")]
+        private static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        private static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetClipboardData(uint uFormat);
+
+        [DllImport("user32.dll")]
+        private static extern bool EmptyClipboard();
+        #endregion
+
+        private const uint CF_TEXT = 1;
+
+        public static string GetSelectedText()
+        {
+            SendKeys.SendWait("^c");
+            Thread.Sleep(100);
+            if (OpenClipboard(IntPtr.Zero))
+            {
+                // Получаем данные из буфера обмена
+                IntPtr hClipboardData = GetClipboardData(CF_TEXT);
+                if (hClipboardData != IntPtr.Zero)
+                {
+                    string selectedText = Marshal.PtrToStringAnsi(hClipboardData);
+                    CloseClipboard();
+                    return selectedText;
+                }
+                CloseClipboard();
+            }
+            return string.Empty;
+        }
+        #endregion
+        private string CorrectText(string text)
+        {
+            // Пример словаря для замены
+            var dictionary = new Dictionary<char, char>
+        {
+            {'q','й'}, {'w','ц'}, {'e','у'}, {'r','к'}, {'t','е'}, {'y','н'}, {'u','г'}, {'i','ш'}, {'o','щ'}, {'p','з'}, {'[','х'},{']','ъ'},
+            {'a','ф'}, {'s','ы'}, {'d','в'}, {'f','а'}, {'g','п'}, {'h','р'}, {'j','о'}, {'k','л'}, {'l','д'}, {';','ж'}, {'\'','э'},
+            {'z','я'}, {'x','ч'}, {'c','с'}, {'v','м'}, {'b','и'}, {'n','т'}, {'m','ь'}, {',','б'}, {'.','ю'}, {'/','.'}, {'?',','},
+            {'@','"'}, {'#','№'}, {'$',';'}, {'^',':'}, {'&','?'}, {' ',' '},
+            {'Q','Й'}, {'W','Ц'}, {'E','У'}, {'R','К'}, {'T','Е'}, {'Y','Н'}, {'U','Г'}, {'I','Ш'}, {'O','Щ'}, {'P','З'}, {'{','Х'}, {'}','Ъ'},
+            {'A','Ф'}, {'S','Ы'}, {'D','В'}, {'F','А'}, {'G','П'}, {'H','Р'}, {'J','О'}, {'K','Л'}, {'L','Д'}, {':','Ж'}, {'"','Э'}, 
+            {'Z','Я'}, {'X','Ч'}, {'C','С'}, {'V','М'}, {'B','И'}, {'N','Т'}, {'M','Ь'}, {'<','Б'}, {'>','Ю'}
+        };
+
+            char[] correctedChars = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                correctedChars[i] = dictionary.ContainsKey(text[i]) ? dictionary[text[i]] : text[i];
+            }
+
+            return new string(correctedChars);
+        }
+        private void translite()
+        {
+            string text = GetSelectedText();
+            if (text != string.Empty)
+            {
+                text = CorrectText(text);
+                tbConclusion.Text = text;
+                Clipboard.SetText(text);
+                SendKeys.SendWait("^v");
+            }
+            else
+            {
+                tbConclusion.Text = "Выделенный текст не найден";
+                sp.Play();
+            }
+        }
+        #endregion
+        #region функционал
+        // трей
+        private void MainForm_Deactivate(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // Передвижение окна (пока что не работает)
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            Message m = Message.Create(base.Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
+            this.WndProc(ref m);
+        }
+        // кнопка свернуть
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        #endregion
+    }
+}
